4b619bb8fb8a78c755775e30bb091ef734a9e235
xcheche <chekwubechukwu23@gmail.com>
2024-08-28 11:26:39 +0100

    done

--- authentication/models.py
+++ authentication/models.py
@@ -1,18 +1,46 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import RegexValidator
 
-from config import settings
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, mobile_number, password=None, **extra_fields):
+        if not mobile_number:
+            raise ValueError("Users must have a mobile number")
+
+        user = self.model(mobile_number=mobile_number,  **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, mobile_number, password=None,  **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(mobile_number, password,  **extra_fields)
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(
+        max_length=11,
+        null=False,
+        unique=True,
+        validators=[
+            RegexValidator(r"^\d{11}$", "Enter a valid 11-digit mobile number.")
+        ],
+    )
+    USERNAME_FIELD = "mobile_number"
+    REQUIRED_FIELDS = ["email"]  # or other required fields if you have them
 
-    USERNAME_FIELD = 'email'
-    if  'auth' in settings.INSTALLED_APPS:
-        AUTH_USER_MODEL = 'auth.User'
-    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
 
     def __str__(self):
-        return self.email
+        return self.mobile_number
